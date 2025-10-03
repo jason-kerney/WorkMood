e3a9b7065208d59637e10c74eaba5b42817135b7
Jason Kerney <jason.kerney@hunterindustries.com>
2025-10-03 14:18:01 -0700

    Remove developer info and icon link functionality from About page and ViewModel

--- MauiApp/ViewModels/AboutViewModel.cs
+++ MauiApp/ViewModels/AboutViewModel.cs
@@ -19,8 +19,8 @@ public class AboutViewModel : ViewModelBase
     private string _appTitle = "WorkMood - Daily Mood Tracker";
     private string _appVersion = string.Empty;
     private string _appDescription = "WorkMood helps you track your daily mood and build healthy emotional awareness habits. Record your feelings throughout the day and view your mood patterns over time.";
-    private string _developerInfo = "Built with ❤️ using .NET MAUI";
-    private string _iconUrl = "https://www.flaticon.com/free-icon/smiles_10949258?term=smiles&page=1&position=41&origin=style&related_id=10949258";
+
+
 
     /// <summary>
     /// Initializes a new instance of the AboutViewModel
@@ -40,7 +40,6 @@ public class AboutViewModel : ViewModelBase
         InitializeVersionInfo();
 
         // Initialize commands
-        OpenIconLinkCommand = new RelayCommand(ExecuteOpenIconLink, CanExecuteOpenIconLink);
     }
 
     #region Properties
@@ -72,23 +71,9 @@ public class AboutViewModel : ViewModelBase
         set => SetProperty(ref _appDescription, value);
     }
 
-    /// <summary>
-    /// Gets the developer information
-    /// </summary>
-    public string DeveloperInfo
-    {
-        get => _developerInfo;
-        set => SetProperty(ref _developerInfo, value);
-    }
 
-    /// <summary>
-    /// Gets the icon attribution URL
-    /// </summary>
-    public string IconUrl
-    {
-        get => _iconUrl;
-        set => SetProperty(ref _iconUrl, value);
-    }
+
+
 
     #endregion
 
@@ -113,41 +98,7 @@ public class AboutViewModel : ViewModelBase
 
     #endregion
 
-    #region Commands
 
-    /// <summary>
-    /// Command to open the icon attribution link
-    /// </summary>
-    public ICommand OpenIconLinkCommand { get; }
 
-    #endregion
 
-    #region Command Implementations
-
-    /// <summary>
-    /// Executes the open icon link command
-    /// </summary>
-    private async void ExecuteOpenIconLink()
-    {
-        try
-        {
-            await _browserService.OpenAsync(IconUrl);
-        }
-        catch (Exception ex)
-        {
-            // Handle the case where the browser couldn't be opened
-            await _navigationService.ShowAlertAsync("Error", $"Unable to open link: {ex.Message}", "OK");
-        }
-    }
-
-    /// <summary>
-    /// Determines if the open icon link command can be executed
-    /// </summary>
-    /// <returns>True if the command can be executed</returns>
-    private bool CanExecuteOpenIconLink()
-    {
-        return !string.IsNullOrEmpty(IconUrl);
-    }
-
-    #endregion
 }
