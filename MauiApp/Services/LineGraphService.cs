be67c02fdb8ec6adfa64d7bfb8d6b1ce2be0de35
Jason Kerney <jason.kerney@hunterindustries.com>
2025-10-03 11:42:39 -0700

    r - extract SaveLineGraphAsync to use file factory for dependency injection

--- MauiApp/Services/LineGraphService.cs
+++ MauiApp/Services/LineGraphService.cs
@@ -898,7 +898,8 @@ public class LineGraphService(IDrawShimFactory drawShimFactory, IFileShimFactory
     public async Task SaveLineGraphAsync(IEnumerable<MoodEntry> moodEntries, DateRange dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, string filePath, GraphMode graphMode, Color lineColor, int width = 800, int height = 600)
     {
         var imageData = await GenerateLineGraphAsync(moodEntries, dateRange, showDataPoints, showAxesAndGrid, showTitle, graphMode, lineColor, width, height);
-        await File.WriteAllBytesAsync(filePath, imageData);
+        var fileShim = fileShimFactory.Create();
+        await fileShim.WriteAllBytesAsync(filePath, imageData);
     }
 
     public async Task SaveLineGraphAsync(IEnumerable<MoodEntry> moodEntries, DateRange dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, string filePath, GraphMode graphMode, string backgroundImagePath, Color lineColor, int width = 800, int height = 600)
