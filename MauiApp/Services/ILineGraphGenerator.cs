7df1a7c0554d354cd2e8503130e4e91b10597de4
Jason Kerney <jason.kerney@hunterindustries.com>
2025-10-09 16:52:45 -0700

    Add title parameter to line graph generation methods for improved customization

--- MauiApp/Services/ILineGraphGenerator.cs
+++ MauiApp/Services/ILineGraphGenerator.cs
@@ -18,10 +18,11 @@ public interface ILineGraphGenerator
     /// <param name="showTrendLine">Whether to show the trend line</param>
     /// <param name="lineColor">Color for the graph line</param>
     /// <param name="yAxisRange">The Y-axis range to use for the graph</param>
+    /// <param name="title">The title to display on the graph (when showTitle is true)</param>
     /// <param name="width">Width of the graph in pixels</param>
     /// <param name="height">Height of the graph in pixels</param>
     /// <returns>PNG image data as byte array</returns>
-    Task<byte[]> GenerateLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, Color lineColor, AxisRange yAxisRange, int width = 800, int height = 600);
+    Task<byte[]> GenerateLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, Color lineColor, AxisRange yAxisRange, string title, int width = 800, int height = 600);
 
     /// <summary>
     /// Generates a line graph PNG image from unified graph data points with custom background
@@ -35,10 +36,11 @@ public interface ILineGraphGenerator
     /// <param name="backgroundImagePath">Path to the custom background image</param>
     /// <param name="lineColor">Color for the graph line</param>
     /// <param name="yAxisRange">The Y-axis range to use for the graph</param>
+    /// <param name="title">The title to display on the graph (when showTitle is true)</param>
     /// <param name="width">Width of the graph in pixels</param>
     /// <param name="height">Height of the graph in pixels</param>
     /// <returns>PNG image data as byte array</returns>
-    Task<byte[]> GenerateLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, string backgroundImagePath, Color lineColor, AxisRange yAxisRange, int width = 800, int height = 600);
+    Task<byte[]> GenerateLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, string backgroundImagePath, Color lineColor, AxisRange yAxisRange, string title, int width = 800, int height = 600);
 
     /// <summary>
     /// Saves a line graph PNG image to the specified file path
@@ -52,10 +54,11 @@ public interface ILineGraphGenerator
     /// <param name="filePath">Path where to save the PNG file</param>
     /// <param name="lineColor">Color for the graph line</param>
     /// <param name="yAxisRange">The Y-axis range to use for the graph</param>
+    /// <param name="title">The title to display on the graph (when showTitle is true)</param>
     /// <param name="width">Width of the graph in pixels</param>
     /// <param name="height">Height of the graph in pixels</param>
     /// <returns>Task representing the async operation</returns>
-    Task SaveLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, string filePath, Color lineColor, AxisRange yAxisRange, int width = 800, int height = 600);
+    Task SaveLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, string filePath, Color lineColor, AxisRange yAxisRange, string title, int width = 800, int height = 600);
 
     /// <summary>
     /// Saves a line graph PNG image to the specified file path with custom background
@@ -70,8 +73,9 @@ public interface ILineGraphGenerator
     /// <param name="backgroundImagePath">Path to the custom background image</param>
     /// <param name="lineColor">Color for the graph line</param>
     /// <param name="yAxisRange">The Y-axis range to use for the graph</param>
+    /// <param name="title">The title to display on the graph (when showTitle is true)</param>
     /// <param name="width">Width of the graph in pixels</param>
     /// <param name="height">Height of the graph in pixels</param>
     /// <returns>Task representing the async operation</returns>
-    Task SaveLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, string filePath, string backgroundImagePath, Color lineColor, AxisRange yAxisRange, int width = 800, int height = 600);
+    Task SaveLineGraphAsync(IEnumerable<GraphDataPoint> dataPoints, DateRangeInfo dateRange, bool showDataPoints, bool showAxesAndGrid, bool showTitle, bool showTrendLine, string filePath, string backgroundImagePath, Color lineColor, AxisRange yAxisRange, string title, int width = 800, int height = 600);
 }
