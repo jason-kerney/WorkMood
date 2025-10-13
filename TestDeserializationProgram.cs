23ac17dadd22f1149252220f67c500def26c3158
Jason Kerney <jason.kerney@hunterindustries.com>
2025-10-13 16:58:32 -0700

    ^r - remove dead code files and unused development scripts

--- TestDeserializationProgram.cs
+++
@@ -1,7 +0,0 @@
-using WorkMood.MauiApp.Tests;
-
-// Run the JSON deserialization test
-await JsonDeserializationTest.TestDeserializationAsync();
-
-Console.WriteLine("\nPress any key to exit...");
-Console.ReadKey();
