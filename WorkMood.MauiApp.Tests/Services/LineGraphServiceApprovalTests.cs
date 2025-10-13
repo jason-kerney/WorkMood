9ccab2e9cef7f7d31d12e7e7a68785abf101eaf7
Jason Kerney <jason.kerney@hunterindustries.com>
2025-10-13 11:59:30 -0700

    refactor LineGraphServiceApprovalTests to use SimpleLineGraphService for generating raw graphs

--- WorkMood.MauiApp.Tests/Services/LineGraphServiceApprovalTests.cs
+++ WorkMood.MauiApp.Tests/Services/LineGraphServiceApprovalTests.cs
@@ -298,8 +298,8 @@ public class LineGraphServiceApprovalTests
         var dateRange = new DateRangeInfo(DateRange.Last7Days, new FakeDateShim(today));
 
         // Act
-        var imageBytes = await _lineGraphService.GenerateRawDataGraphAsync(
-            MoodDataTestHelper.ConvertToRawMoodDataPoints(data),
+        var imageBytes = await _simpleLineGraphService.GenerateRawGraphAsync(
+            data,
             dateRange, 
             showDataPoints: true, 
             showAxesAndGrid: true, 
